780f5acd7e9a6b9d6e3e7fc756fe4ce269b7b119
abbos <abbosvaliev2003@gmail.com>
2023-01-20 17:05:30 +0500

    feat: add story section

--- js/script.js
+++ js/script.js
@@ -22,3 +22,40 @@ const swiperPractic = new Swiper('.practic__swiper', {
     },
   }
 });
+
+
+const swiperStory = new Swiper('.story__swiper', {
+    slidesPerView: 1,
+    autoHeight: true,
+    spaceBetween: 20,
+
+    roundLengths: true,
+    loop: true,
+    loopAdditionalSlides: 30,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints: {
+
+        720: {
+            centeredSlides: false,
+            slidesPerView: 1,
+            roundLengths: false,
+            loop: false,
+            loopAdditionalSlides: 30,
+        },
+
+        1100: {
+            centeredSlides: true,
+            slidesPerView: 3,
+            roundLengths: true,
+            loop: true,
+            loopAdditionalSlides: 30,
+        },
+  }
+});
